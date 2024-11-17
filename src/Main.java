5a1b2422638fafb7a7a7a2d87d18c6f12cdb95b9
Sagi-Five <sagi18001@gmail.com>
2024-11-17 16:25:22 +0200

    mashu

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a number: ");
+        int [] arr = new int [2];
+
+        }
+    }
+}
